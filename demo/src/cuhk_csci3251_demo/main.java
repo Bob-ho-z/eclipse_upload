0fac7821775ec7a8de0ac6eea89fff0e6a571178
hezhanbo <zbhe9@cse.cuhk.edu.hk>
2020-04-21 19:52:07 +0800

    Commit message from bob first time

---
+++ demo/src/cuhk_csci3251_demo/main.java
@@ -0,0 +1,23 @@
+package cuhk_csci3251_demo;
+
+
+import java.util.Scanner;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner myObj = new Scanner(System.in);
+		int number;
+    System.out.println("Hello world");
+    int i ;
+    for(i=0;i<5;i++)
+    {
+     System.out.println("enter number");
+     number= myObj.nextInt();
+     System.out.println(number);
+    }
+	}
+
+}
